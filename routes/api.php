ff97b6c2004b300ea18295dc0ae70d69b4e3be65
Mehti Msv <mehdimsv2001@gmail.com>
2021-01-23 23:00:21 +0330

    Move api routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [App\Http\Controllers\AuthController::class,'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class,'logout']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class,'refresh']);
+    Route::post('me', [App\Http\Controllers\AuthController::class,'me']);
+
+});
+
+Route::middleware('auth:api')->group(function (){
+
+    Route::get("/tweets/my",[App\Http\Controllers\TweetController::class, 'myTweets']);
+    Route::get("/tweets/explore",[App\Http\Controllers\TweetController::class, 'explore']);
+    Route::post("/tweets/create",[App\Http\Controllers\TweetController::class, 'create']);
+    Route::post("/tweets/delete",[App\Http\Controllers\TweetController::class, 'delete']);
+
+    Route::post("/tweets/likeOrNot",[App\Http\Controllers\TweetController::class, 'likeOrNot']);
+    Route::get("/tweets/getLikes",[App\Http\Controllers\TweetController::class, 'getLikes']);
+
+    Route::get("/users/get" ,[App\Http\Controllers\UserController::class, 'getUser']);
+    Route::post("/users/edit" ,[App\Http\Controllers\UserController::class, 'edit']);
+    Route::post("/users/followOrNot" ,[App\Http\Controllers\UserController::class, 'followOrNot']);
+});
--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::prefix("api")->middleware('auth:api')->group(function (){
 
-    Route::get("/tweets/my",[App\Http\Controllers\TweetController::class, 'myTweets']);
-    Route::get("/tweets/explore",[App\Http\Controllers\TweetController::class, 'explore']);
-    Route::post("/tweets/create",[App\Http\Controllers\TweetController::class, 'create']);
-    Route::post("/tweets/delete",[App\Http\Controllers\TweetController::class, 'delete']);
-
-    Route::post("/tweets/likeOrNot",[App\Http\Controllers\TweetController::class, 'likeOrNot']);
-    Route::get("/tweets/getLikes",[App\Http\Controllers\TweetController::class, 'getLikes']);
-
-    Route::get("/users/get" ,[App\Http\Controllers\UserController::class, 'getUser']);
-    Route::post("/users/edit" ,[App\Http\Controllers\UserController::class, 'edit']);
-    Route::post("/users/followOrNot" ,[App\Http\Controllers\UserController::class, 'followOrNot']);
-});
 
 
