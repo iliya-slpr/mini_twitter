5bd92fed820408cb4b549b9f2703fbd7e16170f2
Mehti Msv <mehdimsv2001@gmail.com>
2021-01-23 17:58:15 +0330

    Complete Tweet Model & Controller

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
-    //
+
+    public function myTweets()
+    {
+
+        $tweets = Tweet::whereIn('user_id', [auth()->user()->id])
+            ->with('user')
+            ->withCount('likes')
+            ->latest()
+            ->get();
+
+        return response()->json(["status" => true , "data" => ["tweets" => $tweets]]);
+    }
+
+    public function explore(){
+
+        $users = auth()->user()->following()->get()->pluck("id");
+
+        $tweets = Tweet::whereIn('user_id', $users)
+            ->with('user')
+            ->with('likes')->withCount('likes')
+            ->latest()
+            ->get();
+
+        return response()->json(["status" => true , "data" => ["tweets" => $tweets]]);
+    }
+
+    public function create(Request $request)
+    {
+        $r = $request->user()->tweets()->create([
+            'user_id' => $request->user()->id,
+            'body' => $request->body
+        ]);
+
+        return response()->json(["status" => true , "message" => "Successfully Tweeted"]);
+    }
+
+    public function delete(Request $request)
+    {
+        $tweet = Tweet::find($request->tweet_id);
+        if(is_null($tweet)){
+            $result = ["status" => false , "message" => "Tweet Not Found!"];
+        }else{
+            if($tweet->user_id == auth()->user()->id){
+                $tweet->delete();
+                $result = ["status" => true , "message" => "Successfully Deleted"];
+            }else{
+                $result = ["status" => false , "message" => "You Can't Delete this tweet"];
+            }
+
+        }
+        return response()->json($result);
+    }
+
+    public function likeOrNot(Request $request){
+        $tweet = Tweet::find($request->tweet_id);
+        if(is_null($tweet)) {
+            $result = ["status" => false, "message" => "Tweet Not Found!"];
+        }else{
+            $likeOrNot = $tweet->likes()->toggle(auth()->user()->id);
+
+            if(empty($likeOrNot['attached'])){
+                $message = "Unliked Successfully";
+            }else{
+                $message = "Liked Successfully";
+            }
+            $result = ["status" => true, "message" => $message];
+        }
+
+        return response()->json($result);
+    }
+
+    public function getLikes(Request $request)
+    {
+        $likes = Tweet::find($request->tweet_id)->likes()->get();
+        return response()->json(["status" => true , "data" => ["likes" => $likes]]);
+    }
 }
--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'body',
+    ];
+
+    protected $hidden = [
+      'updated_at'
+    ];
+
+    protected $appends = ['am_i_liked'];
 
     public function likes()
     {
@@ -17,4 +29,13 @@ class Tweet extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function getAmILikedAttribute()
+    {
+        return $this->likes()->get()->contains("id" ,"==",auth()->user()->id );
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
