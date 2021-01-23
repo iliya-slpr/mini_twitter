bb6b43d6c42d3765c2095236e6d1d4177d513804
Mehti Msv <mehdimsv2001@gmail.com>
2021-01-23 17:53:07 +0330

    Add tweets api routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix("api")->middleware('auth:api')->group(function (){
+
+    Route::get("/tweets/my",[App\Http\Controllers\TweetController::class, 'myTweets']);
+    Route::get("/tweets/explore",[App\Http\Controllers\TweetController::class, 'explore']);
+    Route::post("/tweets/create",[App\Http\Controllers\TweetController::class, 'create']);
+    Route::post("/tweets/delete",[App\Http\Controllers\TweetController::class, 'delete']);
+
+    Route::post("/tweets/likeOrNot",[App\Http\Controllers\TweetController::class, 'likeOrNot']);
+    Route::get("/tweets/getLikes",[App\Http\Controllers\TweetController::class, 'getLikes']);
+});
+
+
