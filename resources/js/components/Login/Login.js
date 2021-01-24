f1888f86185dbfc0c2b81160c84f33f98dbbfe9a
iliya-slpr <seilsepour.iliya@gmail.com>
2021-01-24 23:00:12 +0330

    login and register UI Error handling

--- resources/js/components/Login/Login.js
+++ resources/js/components/Login/Login.js
@@ -1,32 +1,60 @@
 import React, { Component } from "react";
-import classes from './Login.module.css'
-import {Link} from 'react-router-dom';
+import classes from "./Login.module.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import swal from 'sweetalert';
+
 class Login extends Component {
-    render() {
-        return(
-        <div className={classes.loginPage}>
-            <div className={classes.form}>
-                {/* <form className={classes.registerForm}>
-                    <input type="text" placeholder="name" />
-                    <input type="password" placeholder="password" />
-                    <input type="text" placeholder="email address" />
-                    <button>create</button>
-                    <p className={classes.message}>
-                        Already registered? <a href="#">Sign In</a>
-                    </p>
-                </form> */}
-                <form className={classes.loginForm}>
-                    <input type="text" placeholder="username" />
-                    <input type="password" placeholder="password" />
-                    <button>login</button>
-                    <p className={classes.message}>
-                        Not registered? <Link to="/register">Create an account</Link>
-                    </p>
-                </form>
-            </div>
-        </div>
-        )
+    constructor(props) {
+        super(props);
+    
+
     }
+    
+
+    submitHandler (e) {
+        e.preventDefault();
+        
+        let data = {email: e.target[0].value , password: e.target[1].value}
+        console.log(data)
+        axios.post('api/auth/login' , data , {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+        .then(res=>{
+            document.cookie=`token=${res.data.access_token}`
+            window.location.href = "/home";
+        })
+       .catch(error => {swal("Error", "Something Wrong! , please Check your input", "error");
+    })
+
+    };
+    
+
+
+    
+    render() {
+        return (
+            <div className={classes.loginPage}>
+                <div className={classes.form}>
+                    <form
+                        onSubmit={this.submitHandler}
+                        className={classes.loginForm}
+                    >
+                        <input
+                            type="text"
+                            onChange={this.chanegeUsername}
+                            placeholder="username"
+                        />
+                        <input type="password" placeholder="password" onChange={this.changePassword}/>
+                        <button>login</button>
+                        <p className={classes.message}>
+                            Not registered?{" "}
+                            <Link to="/register">Create an account</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default Login;
--- resources/js/components/Register/Register.js
+++ resources/js/components/Register/Register.js
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
 import classes from "./Register.module.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import swal from 'sweetalert';
 
 class Register extends Component {
+    constructor(props){
+        super(props);
+       
+
+    }
+    submitHanlder(e){
+        e.preventDefault();
+        let data = {name: e.target[0].value , email: e.target[1].value , password:e.target[2].value , password_confirmation: e.target[3].value}
+        axios.post('/api/auth/register' , data, {headers:{'X-Requested-With': 'XMLHttpRequest'}})
+        .then(res =>{
+            if(res.data.status) window.location.href = "/home";
+            else{
+                let strErrors = '';
+                let errors = res.data.data;
+                for(let att in errors){
+                    strErrors+= errors[att].join('\n');
+                    strErrors+='\n';
+                }
+                
+                swal('Register Failed', strErrors, "error")
+            }
+        })
+        
+    }
     render() {
         return (
             <div className={classes.loginPage}>
                 <div className={classes.form}>
-                    <form className={classes.loginForm}>
+                    <form onSubmit={this.submitHanlder} className={classes.loginForm}>
                         <input type="text" placeholder="username" />
                         <input type="text" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <input type="password" placeholder="Password Again" />
                         <button>Register</button>
                         <p className={classes.message}>
-                            Not registered?{" "}
-                            <Link to="/login">Create an account</Link>
+                            Already registered?{" "}
+                            <Link to="/login">login</Link>
                         </p>
                     </form>
                 </div>
